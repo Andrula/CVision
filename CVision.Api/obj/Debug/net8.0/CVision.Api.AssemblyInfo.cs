3d8a5869fcde2b249b8266c6b1c43754498cc8f5
Andreas <xandreas@live.dk>
2025-10-12 19:32:22 +0200

    Updated GPT model to GPT-4o-mini to save costs and to see what changes in the response. Closes #4

--- CVision.Api/obj/Debug/net8.0/CVision.Api.AssemblyInfo.cs
+++ CVision.Api/obj/Debug/net8.0/CVision.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVision.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382c3908c1d7eb5c58434c1fa29b632a14b56947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bffb7030723e7e8f9062907d8fce94541673845")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVision.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVision.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
